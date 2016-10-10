8eeb2fa6da299e7c36d3064cdb673c86db911dc2
Ara Juljulian <ara@juljulian.com>
2016-10-10 15:35:47 -0700

    Initial Commit

---
+++ RxSwiftExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RxSwiftExample
+//
+//  Created by Ara Juljulian on 10/10/16.
+//  Copyright © 2016 Ara Juljulian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
